3cc051e06284f685f68fc60ebd33c29bc69f37c2
yuanx <yuanx.me@qq.com>
2023-03-27 21:35:26 +0800

    first commit

---
+++ src/pages/api/generateIndictment.ts
@@ -0,0 +1,41 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { ChatGPTAPI, ChatMessage } from "chatgpt";
+
+export type GenerateIndictmentBody = {
+  fact: string;
+  appeal: string;
+};
+
+export interface _NextApiRequest extends NextApiRequest {
+  body: GenerateIndictmentBody;
+}
+
+export default async function handler(
+  req: _NextApiRequest,
+  res: NextApiResponse<ChatMessage>
+) {
+  const {
+    body: { fact, appeal },
+  } = req;
+  const OPENAI_API_KEY = (process.env.OPENAI_API_KEY as string) || "";
+  if (OPENAI_API_KEY === "") {
+    res
+      .status(500)
+      .json({ id: "0", role: "system", text: "请设置 OPENAI_API_KEY" });
+    return;
+  }
+  const api = new ChatGPTAPI({ apiKey: OPENAI_API_KEY });
+  const sendMessage = `
+你是一个金牌律师，专注于解决房屋租赁纠纷，你非常严谨并且精通中国法律，你有良好的职业道德并且充分的站在你的委托人的立场，你可以根据委托人提供的事实描述和诉求为委托人编写符合中国法律的起诉书，在起诉书中请你用清晰的逻辑描述事实缘由，如果有必要请在起诉书中附上相关中国法律条款。
+请你为你的委托人写一封起诉书，以下是你的委托人提供的资料：
+
+事实描述：
+${fact}
+
+诉求：
+${appeal}
+`;
+  const message = await api.sendMessage(sendMessage);
+  res.status(200).json(message);
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,169 @@
+import React, { ChangeEventHandler, useState, forwardRef } from "react";
+import Head from "next/head";
+// import Image from "next/image";
+// import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import { GenerateIndictmentBody } from "./api/generateIndictment";
+import {
+  Button,
+  ButtonToolbar,
+  Form,
+  Input,
+  Message,
+  useToaster,
+  Footer,
+} from "rsuite";
+import { PrependParameters } from "rsuite/esm/@types/utils";
+import { TypeAttributes } from "rsuite/esm/@types/common";
+
+const Textarea = forwardRef((props, ref) => (
+  <Input {...props} as="textarea" ref={ref} className={styles.textarea} />
+));
+
+// const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  const [fact, setFact] = useState("");
+  const [appeal, setAppeal] = useState("");
+  const [indictment, setIndictment] = useState("");
+  const [loading, setLoading] = useState(false);
+  const toaster = useToaster();
+
+  const MyMessage = (content: string, type: TypeAttributes.Status) => {
+    return (
+      <Message showIcon type={type}>
+        {content}
+      </Message>
+    );
+  };
+
+  const generateIndictment = () => {
+    setLoading(true);
+    if (!fact || !appeal) {
+      toaster.push(MyMessage("请输入‘事实经过’和‘诉求’！", "warning"), {
+        placement: "topCenter",
+        duration: 2000,
+      });
+      setLoading(false);
+      return;
+    }
+    const body: GenerateIndictmentBody = {
+      fact: fact,
+      appeal: appeal,
+    };
+    fetch(`${window.location.href}api/generateIndictment`, {
+      method: "post",
+      body: JSON.stringify(body),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => {
+        res.json().then((data) => {
+          console.log("data", data);
+          setIndictment(data.text);
+          setLoading(false);
+        });
+      })
+      .catch((err) => {
+        toaster.push(MyMessage("生成失败，请重试！", "error"), {
+          placement: "topCenter",
+          duration: 2000,
+        });
+        setLoading(false);
+      });
+  };
+
+  const cleanForm = () => {
+    setFact("");
+    setAppeal("");
+  };
+
+  const factChange: PrependParameters<
+    ChangeEventHandler<HTMLInputElement>,
+    [value: string]
+  > = (value, e) => {
+    setFact(value);
+  };
+
+  const appealChange: PrependParameters<
+    ChangeEventHandler<HTMLInputElement>,
+    [value: string]
+  > = (value, e) => {
+    setAppeal(value);
+  };
+
+  const indictmentChange: PrependParameters<
+    ChangeEventHandler<HTMLInputElement>,
+    [value: string]
+  > = (value, e) => {
+    setIndictment(value);
+  };
+
+  return (
+    <>
+      <Head>
+        <title>AI 维权律师</title>
+        <meta name="description" content="AI 维权律师" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.config}>
+          <h1 className={styles.title}>AI 维权律师</h1>
+          <Form fluid className={styles.form}>
+            <Form.Group controlId="textarea">
+              <Form.ControlLabel>事实经过：</Form.ControlLabel>
+              <Form.Control
+                rows={5}
+                name="textarea"
+                accepter={Textarea}
+                value={fact}
+                onChange={factChange}
+              />
+            </Form.Group>
+            <Form.Group controlId="textarea">
+              <Form.ControlLabel>诉求：</Form.ControlLabel>
+              <Form.Control
+                rows={5}
+                name="textarea"
+                accepter={Textarea}
+                value={appeal}
+                onChange={appealChange}
+              />
+            </Form.Group>
+            <Form.Group>
+              <ButtonToolbar>
+                <Button
+                  loading={loading}
+                  appearance="primary"
+                  onClick={generateIndictment}
+                >
+                  生成起诉书
+                </Button>
+                <Button appearance="default" onClick={cleanForm}>
+                  清除数据
+                </Button>
+              </ButtonToolbar>
+            </Form.Group>
+          </Form>
+        </div>
+        <div className={styles.output}>
+          <Textarea
+            placeholder="等待生成起诉书..."
+            value={indictment}
+            onChange={indictmentChange}
+          />
+        </div>
+        <Footer className={styles.footer}>
+          {"yuanx @ "}
+          <a href="https://github.com/imyuanx" target="_blank">
+            GitHub
+          </a>
+          {" | "}
+          <a href="https://twitter.com/imyuanx" target="_blank">
+            Twitter
+          </a>
+        </Footer>
+      </main>
+    </>
+  );
+}
